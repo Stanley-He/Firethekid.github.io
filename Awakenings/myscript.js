636dcd3617d44987580abe4eeddabd20c61bc818
Firethekid <30811717+Firethekid@users.noreply.github.com>
2023-06-13 16:26:10 -0400

    Add files via upload

---
+++ Awakenings/myscript.js
@@ -0,0 +1,118 @@
+$(document).ready(function() {
+    function AwakeningPoints(StarryGems, RetireP) {
+        var Points = 0;
+        for (let Gems = StarryGems; Gems > 99; Gems -= 100) {
+            var odds = Math.random();
+
+            if (odds <= 0.000015) {
+                Points += 19;
+                if (RetireP >= 19) {
+                    Gems += 116667;
+                }
+            } else if (odds <= 0.000040) {
+                Points += 17;
+                if (RetireP >= 17) {
+                    Gems += 25000;
+                }
+            } else if (odds <= 0.000105) {
+                Points += 16;
+                if (RetireP >= 16) {
+                    Gems += 15000;
+                }
+            } else if (odds <= 0.000225) {
+                Points += 15;
+                if (RetireP >= 15) {
+                    Gems += 8000;
+                }
+            } else if (odds <= 0.001000) {
+                Points += 14;
+                if (RetireP >= 14) {
+                    Gems += 1800;
+                }
+            } else if (odds <= 0.006500) {
+                Points += 13;
+                if (RetireP >= 13) {
+                    Gems += 600;
+                }
+            } else if (odds <= 0.022700) {
+                Points += 12;
+                if (RetireP >= 12) {
+                    Gems += 300;
+                }
+            } else if (odds <= 0.044000) {
+                Points += 11;
+                if (RetireP >= 11) {
+                    Gems += 200;
+                }
+            } else if (odds <= 0.087000) {
+                Points += 10;
+                if (RetireP >= 10) {
+                    Gems += 150;
+                }
+            } else if (odds <= 0.131000) {
+                Points += 9;
+                if (RetireP >= 9) {
+                    Gems += 100;
+                }
+            } else if (odds <= 0.179000) {
+                Points += 8;
+                if (RetireP >= 8) {
+                    Gems += 70;
+                }
+            } else if (odds <= 0.271000) {
+                Points += 7;
+                if (RetireP >= 7) {
+                    Gems += 50;
+                }
+            } else if (odds <= 0.471000) {
+                Points += 6;
+                if (RetireP >= 6) {
+                    Gems += 30;
+                }
+            } else if (odds <= 0.759000) {
+                Points += 5;
+                if (RetireP >= 5) {
+                    Gems += 20;
+                }
+            } else if (odds <= 0.957000) {
+                Points += 4;
+                if (RetireP >= 4) {
+                    Gems += 15;
+                }
+            } else {
+                Points += 3;
+                if (RetireP >= 3) {
+                    Gems += 10;
+                }
+            }
+        }
+        return Points;
+    }
+
+    var yes = 0;
+    var no = 0;
+    var total = 0;
+
+    $("#btn_1").click(function() {
+        var Goal = parseInt($("#textEntered1").val()); // Parse the input as an integer
+        var StarryGems = parseInt($("#textEntered2").val()); // Parse the input as an integer
+        var RetireP = parseInt($("#textEntered3").val()); // Parse the input as an integer
+
+        for (var i = 100000; i > 0; i--) {
+            var x = AwakeningPoints(StarryGems, RetireP);
+            total += x;
+            if (x < Goal) {
+                no += 1;
+            } else {
+                yes += 1;
+            }
+        }
+
+        var message = "Successful attempts: " + yes.toString() + "<br>Failed attempts: " + no.toString() + "<br>Success rate: " + (yes*100/100000).toString() + "%<br>Average points: " + (total/100000).toString(); // Use toString() to convert the number to a string
+        $("#textDisplayed1").html(message);
+
+        yes = 0;
+        no = 0;
+        total = 0;
+    });
+});
